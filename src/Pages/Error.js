eb47d6fb223046dbd5dbec7ed4970a97d560a51c
Arthur Chatelain <arthurchtln@gmail.com>
2022-09-27 21:23:42 +0200

    error responsive

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,16 +1,12 @@
 import '../Assets/Style/Pages/Error.css'
 import { Link } from 'react-router-dom'
-import Erreur404 from '../Assets/Images/Erreur404'
-import Header from '../Components/Header'
 
 export default function ErrorPage () {
     return (
         <div className="ErrorPage">
-            <Header pageActive='Error'/>
-            <div className='svgErrorContainer'><Erreur404 /></div>
             <div className='ErrorTextContainer'>
                 <div className='ErrorTextContainer2'>
-                    <p className='ErrorText'>Cette page n'existe pas</p>
+                    <p className='ErrorText'>Erreur 404, Cette page n'existe pas</p>
                     <Link to='/Home' className='ErrorLink'>Retourner à la page d'accueil</Link>
                 </div>
             </div>
