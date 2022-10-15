93790792a5c4622741a9c8fdefcdd4a839c8f37a
Arthur Chatelain <arthurchtln@gmail.com>
2022-10-15 15:23:04 +0200

    accueil texte error resolve

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './Assets/Style/index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, child, get, set } from "firebase/database";
+import { getDatabase, ref, child, get } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAMdElk9Qg3V5RapBZKTIbyugQdmCu1hLg",
@@ -21,13 +21,6 @@ const db = getDatabase(initializeApp(firebaseConfig))
 
 const dbRef = ref(db);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-function setText(name, text) {
-  const db = getDatabase();
-  set(ref(db, 'Textes/' + name), text);
-}
-
-setText('accueil', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec metus tortor, finibus non metus nec, vestibulum pulvinar tortor. Proin iaculis mi ipsum, eu dictum lectus ultricies quis. Phasellus sit amet lectus iaculis, mollis massa et, tincidunt ex. Suspendisse id purus id nisi consectetur ornare.')
 
 get(child(dbRef, `Textes`)).then(snapshot => {
 root.render(
